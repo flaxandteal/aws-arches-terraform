55a256f9c094c50667879518155771db2ab3ddf1
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-18 13:42:29 +0000

    t

--- main.tf
+++ main.tf
@@ -78,140 +78,140 @@ module "vpc" {
   tags = module.labels.tags
 }
 
-# =============================================================================
-# 2. KMS
-# =============================================================================
-module "kms" {
-  source = "./modules/kms"
+# # =============================================================================
+# # 2. KMS
+# # =============================================================================
+# module "kms" {
+#   source = "./modules/kms"
 
-  name        = local.name
-  environment = var.environment
-  #eks_node_role_arns = [module.eks.node_iam_role_arn] # will be known after EKS - nope! circular i think
-  tags = module.labels.tags
+#   name        = local.name
+#   environment = var.environment
+#   #eks_node_role_arns = [module.eks.node_iam_role_arn] # will be known after EKS - nope! circular i think
+#   tags = module.labels.tags
 
-  # We handle the circular dependency cleanly with depends_on
-  #depends_on = [module.eks]
-}
+#   # We handle the circular dependency cleanly with depends_on
+#   #depends_on = [module.eks]
+# }
 
-# =============================================================================
-# 3. EKS – our clean wrapper
-# =============================================================================
-module "eks" {
-  source = "./modules/eks"
+# # =============================================================================
+# # 3. EKS – our clean wrapper
+# # =============================================================================
+# module "eks" {
+#   source = "./modules/eks"
 
-  name_prefix     = var.name_prefix
-  environment     = var.environment
-  cluster_version = var.cluster_version
+#   name_prefix     = var.name_prefix
+#   environment     = var.environment
+#   cluster_version = var.cluster_version
 
-  vpc_id                   = module.vpc.vpc_id
-  private_subnet_ids       = module.vpc.private_subnets
-  control_plane_subnet_ids = length(var.intra_subnet_cidrs) > 0 ? var.intra_subnet_cidrs : module.vpc.private_subnets
+#   vpc_id                   = module.vpc.vpc_id
+#   private_subnet_ids       = module.vpc.private_subnets
+#   control_plane_subnet_ids = length(var.intra_subnet_cidrs) > 0 ? var.intra_subnet_cidrs : module.vpc.private_subnets
 
-  node_instance_type = var.node_instance_type
-  node_min_size      = var.node_min_size
-  node_max_size      = var.node_max_size
-  node_desired_size  = var.node_desired_size
+#   node_instance_type = var.node_instance_type
+#   node_min_size      = var.node_min_size
+#   node_max_size      = var.node_max_size
+#   node_desired_size  = var.node_desired_size
 
-  ebs_kms_key_arn         = module.kms.ebs_kms_key_arn
-  eks_admin_principal_arn = var.eks_admin_principal_arn
-  github_repo             = var.github_repo
-  log_retention_days      = var.log_retention_days
+#   ebs_kms_key_arn         = module.kms.ebs_kms_key_arn
+#   eks_admin_principal_arn = var.eks_admin_principal_arn
+#   github_repo             = var.github_repo
+#   log_retention_days      = var.log_retention_days
 
-  tags = module.labels.tags
+#   tags = module.labels.tags
 
-}
+# }
 
-# =============================================================================
-# 4. IAM – GitHub OIDC
-# =============================================================================
-module "iam" {
-  source = "./modules/iam"
+# # =============================================================================
+# # 4. IAM – GitHub OIDC
+# # =============================================================================
+# module "iam" {
+#   source = "./modules/iam"
 
-  name_prefix = var.name_prefix
-  environment = var.environment
-  github_repo = var.github_repo
-  tags        = module.labels.tags
+#   name_prefix = var.name_prefix
+#   environment = var.environment
+#   github_repo = var.github_repo
+#   tags        = module.labels.tags
 
-}
+# }
 
-# =============================================================================
-# 5. RDS
-# =============================================================================
-module "rds" {
-  source = "./modules/rds"
+# # =============================================================================
+# # 5. RDS
+# # =============================================================================
+# module "rds" {
+#   source = "./modules/rds"
 
-  name_prefix    = var.name_prefix
-  environment    = var.environment
-  vpc_id         = module.vpc.vpc_id
-  db_subnet_ids  = module.vpc.database_subnets
-  eks_node_sg_id = module.eks.node_security_group_id
+#   name_prefix    = var.name_prefix
+#   environment    = var.environment
+#   vpc_id         = module.vpc.vpc_id
+#   db_subnet_ids  = module.vpc.database_subnets
+#   eks_node_sg_id = module.eks.node_security_group_id
 
-  db_class            = var.db_class
-  db_storage          = var.db_storage
-  db_multi_az         = var.db_multi_az
-  db_backup_retention = var.db_backup_retention
-  db_password         = var.db_password
-  kms_key_arn         = module.kms.rds_kms_key_arn
+#   db_class            = var.db_class
+#   db_storage          = var.db_storage
+#   db_multi_az         = var.db_multi_az
+#   db_backup_retention = var.db_backup_retention
+#   db_password         = var.db_password
+#   kms_key_arn         = module.kms.rds_kms_key_arn
 
-  tags = module.labels.tags
+#   tags = module.labels.tags
 
-}
+# }
 
-# =============================================================================
-# 6. S3
-# =============================================================================
-module "s3" {
-  source = "./modules/s3"
+# # =============================================================================
+# # 6. S3
+# # =============================================================================
+# module "s3" {
+#   source = "./modules/s3"
 
-  name                      = local.name
-  environment               = var.environment
-  s3_kms_key_arn            = module.kms.s3_kms_key_arn
-  lifecycle_transition_days = var.lifecycle_transition_days
-  lifecycle_storage_class   = var.lifecycle_storage_class
-  force_destroy             = var.environment != "prod"
+#   name                      = local.name
+#   environment               = var.environment
+#   s3_kms_key_arn            = module.kms.s3_kms_key_arn
+#   lifecycle_transition_days = var.lifecycle_transition_days
+#   lifecycle_storage_class   = var.lifecycle_storage_class
+#   force_destroy             = var.environment != "prod"
 
-  tags = module.labels.tags
-}
+#   tags = module.labels.tags
+# }
 
-# =============================================================================
-# 7. VPC Endpoints – fully private
-# =============================================================================
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = module.vpc.vpc_id
-  service_name      = "com.amazonaws.${var.region}.s3"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids   = module.vpc.private_route_table_ids
+# # =============================================================================
+# # 7. VPC Endpoints – fully private
+# # =============================================================================
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id            = module.vpc.vpc_id
+#   service_name      = "com.amazonaws.${var.region}.s3"
+#   vpc_endpoint_type = "Gateway"
+#   route_table_ids   = module.vpc.private_route_table_ids
 
-  tags = merge(module.labels.tags, {
-    Name = "${local.name}-s3"
-  })
-}
+#   tags = merge(module.labels.tags, {
+#     Name = "${local.name}-s3"
+#   })
+# }
 
-resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id              = module.vpc.vpc_id
-  service_name        = "com.amazonaws.${var.region}.ecr.api"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = module.vpc.private_subnets
-  security_group_ids  = [module.eks.node_security_group_id]
-  private_dns_enabled = true
+# resource "aws_vpc_endpoint" "ecr_api" {
+#   vpc_id              = module.vpc.vpc_id
+#   service_name        = "com.amazonaws.${var.region}.ecr.api"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = module.vpc.private_subnets
+#   security_group_ids  = [module.eks.node_security_group_id]
+#   private_dns_enabled = true
 
-  tags = merge(module.labels.tags, {
-    Name = "${local.name}-ecr-api"
-  })
-}
+#   tags = merge(module.labels.tags, {
+#     Name = "${local.name}-ecr-api"
+#   })
+# }
 
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id              = module.vpc.vpc_id
-  service_name        = "com.amazonaws.${var.region}.ecr.dkr"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = module.vpc.private_subnets
-  security_group_ids  = [module.eks.node_security_group_id]
-  private_dns_enabled = true
+# resource "aws_vpc_endpoint" "ecr_dkr" {
+#   vpc_id              = module.vpc.vpc_id
+#   service_name        = "com.amazonaws.${var.region}.ecr.dkr"
+#   vpc_endpoint_type   = "Interface"
+#   subnet_ids          = module.vpc.private_subnets
+#   security_group_ids  = [module.eks.node_security_group_id]
+#   private_dns_enabled = true
 
-  tags = merge(module.labels.tags, {
-    Name = "${local.name}-ecr-dkr"
-  })
-}
+#   tags = merge(module.labels.tags, {
+#     Name = "${local.name}-ecr-dkr"
+#   })
+# }
 
 # # Add the rest only if you really need them (most clusters work fine with just S3 + ECR)
 # resource "aws_vpc_endpoint" "ssm" {
