d1c62b449df87ce77a9a01b729427aabaa9784e4
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-18 17:07:08 +0000

    t

--- variables.tf
+++ variables.tf
@@ -116,6 +116,7 @@ variable "node_max_size" {
 variable "node_desired_size" {
   description = "Desired number of worker nodes"
   type        = number
+  default     = 1
 }
 
 variable "log_retention_days" {
