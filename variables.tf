105c8d51fb5357e4932e61b821c1f57e2b9c1f44
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-18 17:38:53 +0000

    t

--- variables.tf
+++ variables.tf
@@ -60,11 +60,13 @@ variable "vpc_azs" {
 variable "app_subnet_cidrs" {
   description = "CIDR blocks for application (EKS node) private subnets"
   type        = list(string)
+  default = []
 }
 
 variable "db_subnet_cidrs" {
   description = "CIDR blocks for isolated database subnets"
   type        = list(string)
+  default = []
 }
 
 variable "intra_subnet_cidrs" {
