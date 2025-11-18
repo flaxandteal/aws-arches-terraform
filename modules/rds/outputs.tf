b1b4ce08c033d0b8de1e24d5adc5f975841362d3
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-18 12:32:34 +0000

    t

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -1,14 +1,35 @@
+# modules/rds/outputs.tf
 output "endpoint" {
-  description = "RDS instance endpoint"
+  description = "The connection endpoint for the RDS instance"
   value       = module.rds.db_instance_endpoint
 }
 
+output "address" {
+  description = "The hostname of the RDS instance"
+  value       = module.rds.db_instance_address
+}
+
+output "port" {
+  description = "The database port"
+  value       = module.rds.db_instance_port
+}
+
+output "arn" {
+  description = "ARN of the RDS instance"
+  value       = module.rds.db_instance_arn
+}
+
+output "id" {
+  description = "The RDS instance ID (identifier)"
+  value       = module.rds.db_instance_id
+}
+
 output "security_group_id" {
-  description = "Security group ID attached to RDS"
+  description = "Security group ID attached to the RDS instance"
   value       = aws_security_group.rds.id
 }
 
-output "instance_id" {
-  description = "RDS instance identifier"
-  value       = module.rds.db_instance_id
+output "resource_id" {
+  description = "The RDS Resource ID (useful for CloudWatch)"
+  value       = module.rds.db_instance_resource_id
 }
