cc9bc833d477f92f3ce2384bfb00efabf811ea04
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-19 12:07:07 +0000

    t

--- main.tf
+++ main.tf
@@ -19,7 +19,6 @@ terraform {
       version = "~> 1.19"
     }
   }
-  experiments = [deprecation_warnings_suppression] #silence deprecated syntax outside our control
 
   backend "s3" {} # config injected at runtime from GitHub secret
 }
