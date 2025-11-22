6e9a6853819e7b1163a62c229769969f25828cbf
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-22 19:09:58 +0000

    t

--- outputs.tf
+++ outputs.tf
@@ -12,13 +12,13 @@ output "github_actions_role_arn" { value = module.iam.github_actions_role_arn }
 output "rds_endpoint" { value = module.rds.endpoint }
 
 # root/outputs.tf
-output "debug_vpc_and_subnets" {
-  value = {
-    eks_vpc_id          = module.vpc.vpc_id
-    eks_private_subnets = module.vpc.private_subnet_ids
-    rds_vpc_id          = module.vpc.vpc_id
-    rds_subnet_ids      = module.vpc.private_subnet_ids
-    rds_security_group  = module.rds.aws_security_group.rds.id
-    eks_node_sg         = module.eks.node_security_group_id
-  }
-}
+# output "debug_vpc_and_subnets" {
+#   value = {
+#     eks_vpc_id          = module.vpc.vpc_id
+#     eks_private_subnets = module.vpc.private_subnet_ids
+#     rds_vpc_id          = module.vpc.vpc_id
+#     rds_subnet_ids      = module.vpc.private_subnet_ids
+#     rds_security_group  = module.rds.aws_security_group.rds.id
+#     eks_node_sg         = module.eks.node_security_group_id
+#   }
+# }
