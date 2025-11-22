c3a2dae68d036636c24da51dbeff04b6102606c5
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-22 20:26:32 +0000

    t

--- outputs.tf
+++ outputs.tf
@@ -14,15 +14,15 @@ output "rds_endpoint" { value = module.rds.endpoint }
 
 # root/outputs.tf
 
-output "debug_vpc_and_subnets" {
-  description = "Confirm EKS and RDS are in the same VPC/subnets"
-  value = {
-    vpc_id                     = module.vpc.vpc_id
-    private_subnet_ids         = module.vpc.private_subnet_ids
-    eks_cluster_name           = module.eks.cluster_name
-    eks_node_security_group_id = module.eks.node_security_group_id
-    rds_security_group_id      = module.rds.aws_security_group.rds.id
-    rds_subnet_group_name      = module.rds.module.rds.db_subnet_group_name
-    rds_instance_endpoint      = module.rds.module.rds.db_instance_endpoint
-  }
-}
+# output "debug_vpc_and_subnets" {
+#   description = "Confirm EKS and RDS are in the same VPC/subnets"
+#   value = {
+#     vpc_id                     = module.vpc.vpc_id
+#     private_subnet_ids         = module.vpc.private_subnet_ids
+#     eks_cluster_name           = module.eks.cluster_name
+#     eks_node_security_group_id = module.eks.node_security_group_id
+#     rds_security_group_id      = module.rds.aws_security_group.rds.id
+#     rds_subnet_group_name      = module.rds.module.rds.db_subnet_group_name
+#     rds_instance_endpoint      = module.rds.module.rds.db_instance_endpoint
+#   }
+# }
