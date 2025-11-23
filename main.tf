c5e304e4e34d871dc5fa4b3a6472c33b5e25e30c
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-23 19:47:47 +0000

    t

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ module "vpc" {
   cidr = var.vpc_cidr
   azs  = var.vpc_azs
 
-  s3_logging_bucket_arn = module.s3_logging_bucket.bucket_arn
+  s3_logging_bucket_arn = "arn:aws:s3:::${module.s3_logging_bucket.bucket}"
   account_id            = data.aws_caller_identity.current.account_id
 }
 
