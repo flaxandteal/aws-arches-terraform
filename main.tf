76efccd2b6f03b22910326be1661e5d72b9defe2
SharonFT <sharon.ingram@flaxandteal.co.uk>
2025-11-26 10:03:34 +0000

    t

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ module "s3" {
   lifecycle_transition_days = var.lifecycle_transition_days
   lifecycle_storage_class   = var.lifecycle_storage_class
   force_destroy             = var.environment != "prod"
-  logging_bucket            = module.s3_logging_bucket.bucket_name
+  logging_bucket            = module.s3_logging_bucket.bucket_arn
 
   tags = module.labels.tags
 
--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -5,3 +5,15 @@ output "bucket_arn" { value = aws_s3_bucket.this.arn }
 output "bucket_regional_domain_name" {
   value = aws_s3_bucket.this.bucket_regional_domain_name
 }
+output "bucket_region" {
+  description = "The AWS region of the bucket"
+  value       = aws_s3_bucket.this.region
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.this.bucket_domain_name
+}
+output "bucket_id" {
+  description = "Same as bucket_name (kept for backward compatibility)"
+  value       = aws_s3_bucket.this.id
+}
