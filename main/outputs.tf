17cb142dd48623c37891721a8bac08c6a42bdee9
Andrea K. <andrea.kyurchiev@flaxandteal.co.uk>
2026-09-17 11:55:32 +0300

    feat: add ECR repos and OIDC roles for catalina-build push and Flux image scanning

--- main/outputs.tf
+++ main/outputs.tf
@@ -25,3 +25,31 @@ output "s3_prebuild_bucket_name" {
 output "starches_ci_role_arn" {
   value = aws_iam_role.starches_ci.arn
 }
+
+output "catalina_build_ecr_push_role_arn" {
+  value = aws_iam_role.catalina_build_ecr_push.arn
+}
+
+output "ecr_catalina_arches_repository_url" {
+  value = module.ecr_catalina_arches.repository_url
+}
+
+output "ecr_catalina_arches_static_repository_url" {
+  value = module.ecr_catalina_arches_static.repository_url
+}
+
+output "ecr_catalina_arches_static_py_repository_url" {
+  value = module.ecr_catalina_arches_static_py.repository_url
+}
+
+output "ecr_catalina_starches_repository_url" {
+  value = module.ecr_catalina_starches.repository_url
+}
+
+output "ecr_catalina_starches_private_repository_url" {
+  value = module.ecr_catalina_starches_private.repository_url
+}
+
+output "flux_image_reflector_role_arn" {
+  value = aws_iam_role.flux_image_reflector.arn
+}
